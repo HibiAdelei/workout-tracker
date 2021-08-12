076c99983d38ccd80a697e030538fb9324606a37
HibiAdelei <lilyadelame@gmail.com>
2021-08-11 21:51:18 -0700

    deploy

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const router = require("./routes/apiRoutes.js");
-
 
 const PORT = process.env.PORT || 3001;
 
-// Create Express app
+// creating Express app
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+const MONGODB_URI = 'mongodb+srv://merrypassword:merrypassword@workoutdb.t2nup.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-// Create mongoose database connection
+// creating mongoose database connection
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected.');
 });
 
 // routes
